efb372f569c6a838b4269e9d9caa1b8198656adb
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-08-01 21:28:54 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    indent: ["error", 4],
+    "space-before-function-paren": 0,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-useless-constructor": "off"
+  }
+}
